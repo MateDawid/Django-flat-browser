ffb25797a5456684b402e309e12c4853e2f5205c
Dawid Mateusiak <mateusiakdawid@gmail.com>
2021-01-25 00:15:34 +0100

    Update template, add model SearchingResult, modify views

--- flatbrowser/main/views.py
+++ flatbrowser/main/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
+from datetime import datetime
+from datetime import timedelta
 
-from .models import Flat
+from .models import Flat, SearchingResult
 from .forms import SearchForm
 from offer_searching_engine.scrapping_functions import scrap_otodom, scrap_morizon
 from offer_searching_engine.allegro_api_functions import get_from_allegro_api
 
 def search_flats(request):
     form = SearchForm(request.POST or None, request.FILES or None)
-    flats_found = Flat.objects.all()
+    flats_found = []
     if form.is_valid():
+        user_search = SearchingResult()
+        user_search.save()
         city = form.data["city"]
         min_price = form.data["min_price"]
         max_price = form.data["max_price"]
@@ -18,8 +22,8 @@ def search_flats(request):
         found_offers = scrap_otodom(city,min_price,max_price,min_area,max_area,days_from_publication) + scrap_morizon(city,min_price,max_price,min_area,max_area,days_from_publication) + get_from_allegro_api(city,min_price,max_price,min_area,max_area,days_from_publication)
         
         for offer in found_offers:
-            if Flat.objects.filter(url=offer["url"]).count() >= 1:
-                continue
+            if Flat.objects.filter(image=offer["image"]).count() >= 1:
+                user_search.results.add(Flat.objects.get(image=offer["image"]))
             else:
                 new_flat = Flat() 
                 new_flat.site = offer["site"]
@@ -30,8 +34,10 @@ def search_flats(request):
                 new_flat.url = offer["url"]
                 new_flat.image = offer["image"]
                 new_flat.save()
+                user_search.results.add(new_flat)
         
-        flats_found = Flat.objects.all()
+
+        flats_found = user_search.results.all()
         return render(request, 'main/result.html', {"form":form, "flats_found":flats_found})
     return render(request, 'main/result.html', {"form":form, "flats_found":flats_found })
 
--- flatbrowser/offer_searching_engine/allegro_api_functions.py
+++ flatbrowser/offer_searching_engine/allegro_api_functions.py
@@ -33,7 +33,7 @@ def get_refresh_token():
 
 def get_offer_details(data, offer_type, offer_number):
     offer_title = data['items'][offer_type][offer_number]['name']
-    area = 0
+    area = '0'
     price = data['items'][offer_type][offer_number]['sellingMode']['price']['amount']
     URL = data['items'][offer_type][offer_number]['vendor']['url']
     image = data['items'][offer_type][offer_number]['images'][0]['url']
@@ -50,7 +50,7 @@ def get_available_offers(data, city):
         except:
             continue
     for offer_number in range(len(data['items']['promoted'])):
-        try:
+        try:    
             offer = get_offer_details(data, 'promoted', offer_number)
             offer["site"] = "allegro.pl"
             offer["city"] = city.title()
@@ -82,7 +82,11 @@ def get_from_allegro_api(city, price_from='', price_to='', area_from='', area_to
             response = session.get(DEFAULT_API_URL + 'offers/listing', params=paramethers)
             data = response.json()
             offers = get_available_offers(data, city)
+            print(offers)
             return offers
         else:
             offers = get_available_offers(data, city)
+            print(offers)
             return offers
+
+get_from_allegro_api('Warszawa')
