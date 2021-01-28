6f3de4d98614cd3154a1bbfd4d5a263485762d38
Dawid Mateusiak <mateusiakdawid@gmail.com>
2021-01-29 00:15:14 +0100

    Fix API authentication, add README

--- README.md
+++ README.md
@@ -4,29 +4,65 @@
 * [General info](#general-info)
 * [Screenshots](#screenshots)
 * [Technologies](#technologies)
+* [How it work?](#how-it-works?)
+* [Setup](#setup)
 * [Features](#features)
 * [Status](#status)
 * [Contact](#contact)
 
-## General info
-I made this application while I was looking for an apartment to buy. I figured it takes a long time to check each page separately and I decided to shorten the process. This program enables faster and easier viewing of housing websites.
-
 ## Screenshots
 
+* Home page
+![Example screenshot](./img/1.png)
+
+* Results page
+![Example screenshot](./img/2.png)
+
+* Watchlist page
+![Example screenshot](./img/3.png)
+
+* Registration page
+![Example screenshot](./img/4.png)
 
 ## Technologies
 * Python 3.8.1
-* Django
+* Django 3.1.5
 * BeautifulSoup4
+* Bootstrap
 * requests
-* Tkinter
 * datetime
 * relativedelta
 
+## How it works?
+Application collects data in two ways from three sources:
+* By scrapping offers using BeautifulSoup4 from otodom.pl and morizon.pl,
+* By getting offers from Allegro API.
+
+To start collecting data user needs to fullfil search form. Found offers are saved in database (if they're not already there) and displayed.
+
+Displaying is different depending on user is logged in or not. If user have logged in displayed offers are possible to be saved in or deleted from watchlist. In other case there's no such option.
+
+## Setup
+To install and run app you will need to:
+* Clone this repository to your computer or unpack .zip file in chosen directory
+* Run IDE or command line
+* Command in IDE to install demanded packages:
+> pip install -r requirements.txt
+* Run comand line
+* Type "cd YOUR_DIRECTORY\flatbrowser" in command line
+* Type "python manage.py runserver 8000" in command line
+* Type "http://127.0.0.1:8000/" in your webbrowser.
+
 ## Features
 
-## Status
-In progress.
+* Function of scrapping data from otodom.pl and morizon.pl using BeautifulSoup4,
+* Function of getting data from Allegro API,
+* Displaying data on website basing on user paramethers (city, price, area, days from publication)
+* User registration, login and logout,
+* Displaying user's watchlist, 
+* Adding to and deleting offers from watchlist.
 
+## Status
+Ready to use. Needs to be uploaded to Heroku.
 ## Contact
 Created by [@matedawid](https://linkedin.com/in/matedawid) - if you have any questions, just contact me!
--- flatbrowser/offer_searching_engine/allegro_api_functions.py
+++ flatbrowser/offer_searching_engine/allegro_api_functions.py
@@ -2,62 +2,122 @@ import requests
 import json
 import os.path
 import pathlib
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 DEFAULT_OAUTH_URL = 'https://allegro.pl/auth/oauth'
 DEFAULT_API_URL = 'https://api.allegro.pl/'
+DEFAULT_REDIRECT_URI = 'http://localhost:8000'
 client = "1314c86f6d4c421b8738c2438073533b"
 secret = "i7MkPimYuHmxEzj7vsx0x6oPPHbuzm13HczpB0eSMOY82dxlTA1i2G1d6tHdsUZZ"
 
-def refresh_token(client_id, client_secret, refresh_token, oauth_url=DEFAULT_OAUTH_URL):
-    token_url = oauth_url + '/token'
+def get_access_code(client_id, redirect_uri=DEFAULT_REDIRECT_URI, oauth_url=DEFAULT_OAUTH_URL):
+    auth_url = '{}/authorize' \
+               '?response_type=code' \
+               '&client_id={}' \
+               '&redirect_uri={}'.format(oauth_url, client_id, redirect_uri)
+ 
+    parsed_redirect_uri = requests.utils.urlparse(redirect_uri)
+    server_address = parsed_redirect_uri.hostname, parsed_redirect_uri.port
 
-    access_token_data = {'grant_type': 'refresh_token',
-                         'refresh_token': refresh_token}
+    class AllegroAuthHandler(BaseHTTPRequestHandler):
+        def __init__(self, request, address, server):
+            super().__init__(request, address, server)
+
+        def do_GET(self):
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+
+            self.server.path = self.path
+            self.server.access_code = self.path.rsplit('?code=', 1)[-1]
+
+    webbrowser.open(auth_url)
+    httpd = HTTPServer(server_address, AllegroAuthHandler)
+    httpd.handle_request()
+    httpd.server_close()
+    _access_code = httpd.access_code
+    return _access_code
+
+def sign_in(client_id, client_secret, access_code, redirect_uri=DEFAULT_REDIRECT_URI, oauth_url=DEFAULT_OAUTH_URL):
+    token_url = oauth_url + '/token'
+    access_token_data = {'grant_type': 'authorization_code',
+                         'code': access_code,
+                         'redirect_uri': redirect_uri}
 
     response = requests.post(url=token_url,
                              auth=requests.auth.HTTPBasicAuth(client_id, client_secret),
                              data=access_token_data)
-
+    
     with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json'), 'w') as json_file:
         json.dump(response.json(), json_file)
+    return response.json()
+
+def refresh_token(client_id, client_secret, refresh_token, oauth_url=DEFAULT_OAUTH_URL):
+    try:
+        token_url = oauth_url + '/token'
+
+        access_token_data = {'grant_type': 'refresh_token',
+                            'refresh_token': refresh_token}
+
+        response = requests.post(url=token_url,
+                                auth=requests.auth.HTTPBasicAuth(client_id, client_secret),
+                                data=access_token_data)
+
+        with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json'), 'w') as json_file:
+            json.dump(response.json(), json_file)
+    except:
+        pass
 
 def get_current_token():
-    with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json')) as f:
-        data = json.load(f)
-        return data['access_token']
+    try:
+        with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json')) as f:
+            data = json.load(f)
+            return data['access_token']
+    except:
+        pass
 
 def get_refresh_token():
-    with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json')) as f:
-        data = json.load(f)
-        return data['refresh_token']
+    try:
+        with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'access.json')) as f:
+            data = json.load(f)
+            return data['refresh_token']
+    except:
+        pass
 
 def get_offer_details(data, offer_type, offer_number):
-    offer_title = data['items'][offer_type][offer_number]['name']
-    price = data['items'][offer_type][offer_number]['sellingMode']['price']['amount']
-    URL = data['items'][offer_type][offer_number]['vendor']['url']
-    image = data['items'][offer_type][offer_number]['images'][0]['url']
-    return {"title": offer_title,"price": int(float(price)),"url": URL, "image": image}
+    try:
+        offer_title = data['items'][offer_type][offer_number]['name']
+        price = data['items'][offer_type][offer_number]['sellingMode']['price']['amount']
+        URL = data['items'][offer_type][offer_number]['vendor']['url']
+        image = data['items'][offer_type][offer_number]['images'][0]['url']
+        return {"title": offer_title,"price": int(float(price)),"url": URL, "image": image}
+    except:
+        pass
 
 def get_available_offers(data, city, area_from):
     offers = []
-    for offer_number in range(len(data['items']['regular'])):
-        try:
-            offer = get_offer_details(data, 'regular', offer_number)
-            offer["area"] = area_from
-            offer["site"] = "allegro.pl"
-            offer["city"] = city.title()
-            offers.append(offer)
-        except:
-            continue
-    for offer_number in range(len(data['items']['promoted'])): 
-        try:
-            offer = get_offer_details(data, 'promoted', offer_number)
-            offer["site"] = "allegro.pl"
-            offer["city"] = city.title()
-            offers.append(offer)
-        except:
-            continue
-    return offers
+    try:
+        for offer_number in range(len(data['items']['regular'])):
+            try:
+                offer = get_offer_details(data, 'regular', offer_number)
+                offer["area"] = area_from
+                offer["site"] = "allegro.pl"
+                offer["city"] = city.title()
+                offers.append(offer)
+            except:
+                continue
+        for offer_number in range(len(data['items']['promoted'])): 
+            try:
+                offer = get_offer_details(data, 'promoted', offer_number)
+                offer["site"] = "allegro.pl"
+                offer["city"] = city.title()
+                offers.append(offer)
+            except:
+                continue
+        return offers
+    except:
+        return offers
 
 
 def get_from_allegro_api(city, price_from='', price_to='', area_from=0, area_to='', days=''):
@@ -72,17 +132,20 @@ def get_from_allegro_api(city, price_from='', price_to='', area_from=0, area_to=
 
     with requests.Session() as session:
         session.headers.update(headers)
-        response = session.get(DEFAULT_API_URL + 'offers/listing', params=paramethers)
-        data = response.json()
-
-        if response.status_code == 401:
-            refresh_token(client, secret, get_refresh_token())
-            headers['Authorization'] = "Bearer {}".format(get_current_token())
-            session.headers.update(headers)
+        try:
             response = session.get(DEFAULT_API_URL + 'offers/listing', params=paramethers)
+            
+            if response.status_code == 401:
+                refresh_token(client, secret, get_refresh_token())
+                headers['Authorization'] = "Bearer {}".format(get_current_token())
+                session.headers.update(headers)
+                response = session.get(DEFAULT_API_URL + 'offers/listing', params=paramethers)
+                
+                if response.status_code == 401:
+                    sign_in(client, secret, get_access_code(client))
+
             data = response.json()
             offers = get_available_offers(data, city, area_from)
             return offers
-        else:
-            offers = get_available_offers(data, city, area_from)
-            return offers
+        except:
+            pass
