0aa765b29f56322c7bd90699b694522b6c735e73
Dawid Mateusiak <mateusiakdawid@gmail.com>
2021-01-31 22:07:59 +0100

    Another Heroku fix

--- flatbrowser/flatbrowser/settings.py
+++ flatbrowser/flatbrowser/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/main')
+
+django_heroku.settings(locals())
