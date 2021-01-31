c0e79b16e480819e293787d1dd7a4b74ba31d922
Dawid Mateusiak <mateusiakdawid@gmail.com>
2021-01-31 21:01:35 +0100

    Getting back to SQLite

--- flatbrowser/flatbrowser/settings.py
+++ flatbrowser/flatbrowser/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'flatbrowser.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django_flatbrowser',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_flatbrowser',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
